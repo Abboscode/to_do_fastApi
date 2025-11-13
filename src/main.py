d3d58b50aa4b981c7f3160cea88cbf2b5f80148b
Abbosjon Karshiev <s284350@studenti.polito.it>
2025-11-13 14:49:07 +0100

    feature:signIn and singout  jinja templates added and styled with bootstrap

--- src/main.py
+++ src/main.py
@@ -2,13 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from todo.router import route as todo_router
+
 app = FastAPI(title="To-Do Application", description="A simple to-do application using FastAPI and Jinja2", version="1.0.0")
-templates= Jinja2Templates(directory="templates")
+# Initialize Jinja2 templates
+templates= Jinja2Templates(directory="../templates")
+
+
 data=[{"id":1,"task":"Do laundry"},{"id":2,"task":"Read a book"},{"id":3,"task":"Write code"},{"id":4,"task":"Go for a walk"},{"id":5,"task":"Cook dinner"}]
 
 @app.get("/",tags=["root"])
 async def read_root(request: Request):
-    return ({"message": "Welcome to the To-Do Application!"})
+    return (templates.TemplateResponse("index.html", {"request": request}))
 
 app.include_router(todo_router)
 if __name__ == "__main__":
